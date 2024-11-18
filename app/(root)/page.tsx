51b721c88e1ed4b6632affd8a99b04936f03da61
silvandario <silvan.dario@hotmail.com>
2024-11-18 20:26:33 +0100

    Fix type declaration for sharableId, add Radix UI Tabs component, implement BankTabItem and BankInfo components, enhance DoughnutChart with dynamic data, and create Pagination component for navigation

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,11 +1,23 @@
 import HeaderBox from '@/components/HeaderBox'
+import RecentTransactions from '@/components/RecentTransactions';
 import RightSidebar from '@/components/RightSidebar'
 
 import { TotalBalanceBox } from '@/components/TotalBalanceBox';
+import { getAccount, getAccounts } from '@/lib/actions/bank.actions';
 import { getLoggedInUser } from '@/lib/actions/user.actions';
 
-const Home = async () => {
-    const loggedIn = await getLoggedInUser(); ;
+const Home = async ({ searchParams: { id, page } }: SearchParamProps ) => {
+    const currentPage = Number(page as string) || 1;
+    const loggedIn = await getLoggedInUser();
+    const accounts = await getAccounts({ 
+      userId: loggedIn.$id 
+    })
+    if(!accounts) return;
+    const accountsData = accounts?.data;
+    const appwriteItemId = (id as string) || accountsData[0]?.appwriteItemId;
+  
+    const account = await getAccount({ appwriteItemId })
+
   return (
     <section className="home">
       <div className="home-content">
@@ -13,17 +25,24 @@ const Home = async () => {
                 <HeaderBox 
                     type="greeting"
                     title="Welcome"
-                    user={ loggedIn?.name || "Guest"}
+                    user={ loggedIn?.firstName || "Guest"}
                     subtext="Manage your finances with ease" 
                 />
                 <TotalBalanceBox 
-                accounts = {[]}
-                totalBanks = {1}
-                totalCurrentBalance = {43234.90}
-                />
+                  accounts={accountsData}
+                  totalBanks={accounts?.totalBanks}
+                  totalCurrentBalance={accounts?.totalCurrentBalance}
+          />
             
             </header>
-            RECENT TRANSACTIONS
+            
+            <RecentTransactions 
+              accounts={accountsData}
+              transactions={account?.transactions}
+              appwriteItemId={appwriteItemId}
+              page={currentPage}
+            />
+
         </div>
         <RightSidebar
         user={loggedIn}
