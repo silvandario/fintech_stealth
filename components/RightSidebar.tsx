41a1451e8f228ecc3611ea3e0f4972eedb382831
silvandario <silvan.dario@hotmail.com>
2024-11-18 21:00:16 +0100

    Refactor layout components, add Radix UI Select and Progress components, implement transaction history pagination, and enhance bank account management features

--- components/RightSidebar.tsx
+++ components/RightSidebar.tsx
@@ -5,6 +5,7 @@ import BankCard from './BankCard'
 import { countTransactionCategories } from '@/lib/utils'
 import Category from './Category'
 
+
 const RightSidebar = ({ user, transactions, banks }: RightSidebarProps) => {
   const categories: CategoryCount[] = countTransactionCategories(transactions);
 
