17f2d1751e8a25923e136423f382e6c9d2d4d283
silvandario <silvan.dario@hotmail.com>
2024-11-19 07:46:08 +0100

    Entferne Sidebar-Komponente aus dem Layout und setze dynamische Rendering-Eigenschaft auf 'force-dynamic'

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,6 +1,5 @@
 import MobileNav from "@/components/MobileNav";
 import RightSidebar from "@/components/RightSidebar";
-import Sidebar from "@/components/Sidebar";
 import { getLoggedInUser } from "@/lib/actions/user.actions";
 import Image from "next/image";
 import { redirect } from "next/navigation";
